07e3aca90f68a62e128e54719cd85afcc1e20a83
andreujavi <36564458+andreujavi@users.noreply.github.com>
2026-08-08 10:05:29 +0200

    Refactor server.js for improved Socket.io setup

--- server.js
+++ server.js
@@ -4,14 +4,12 @@ import { Server } from 'socket.io';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-// Truco necesario en el formato moderno para encontrar la carpeta 'public'
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
 const server = http.createServer(app);
 
-// 1. Configuración de Socket.io blindada y con puertas abiertas (CORS)
 const io = new Server(server, {
     cors: {
         origin: "*",
@@ -19,10 +17,8 @@ const io = new Server(server, {
     }
 });
 
-// 2. Le decimos que entregue la carpeta de diseño (public)
 app.use(express.static(path.join(__dirname, 'public')));
 
-// 3. Lógica del tiempo real
 io.on('connection', (socket) => {
     console.log('✅ Nuevo dispositivo conectado');
 
@@ -40,7 +36,6 @@ io.on('connection', (socket) => {
     });
 });
 
-// 4. Arranque del servidor compatible con Render
 const PUERTO = process.env.PORT || 3000;
 server.listen(PUERTO, '0.0.0.0', () => {
     console.log(`🚀 Servidor GastroPOS funcionando en el puerto ${PUERTO}`);
